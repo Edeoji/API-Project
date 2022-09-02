6d65701df2db7bc1dca4f6a2b02a183f02a19817
Edeoji <edeojiemmanuel123@gmail.com>
2022-09-02 14:07:27 +0100

    Migration And model  Added

---
+++ TopRankHotel/Core/Migrations/20220823135858_addUserverifyToDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Migrations
+{
+    public partial class addUserverifyToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Userverifications",
+                columns: table => new
+                {
+                    Token = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Used = table.Column<bool>(type: "bit", nullable: false),
+                    DateUsed = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Userverifications", x => x.Token);
+                    table.ForeignKey(
+                        name: "FK_Userverifications_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Userverifications_UserId",
+                table: "Userverifications",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Userverifications");
+        }
+    }
+}
---
+++ TopRankHotel/Core/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string firstNmae { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Address { get; set; }
+        public string Picture { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public bool Activated { get; set; }
+        public bool Deleted { get; set; }
+        public string Nationality { get; set; }
+        public string State { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public bool RememberPassword { get; set; }
+        public string Description { get; set; }
+    }
+}
